1684bb48a2fbde14a8b4ffa0bf6d7c659ad18f7e
joao-vitorg <jovitor737@gmail.com>
2022-12-16 16:50:38 -0300

    Computer intelligence

--- js/script.js
+++ js/script.js
@@ -1,26 +1,25 @@
 let playerTurn = 0,
     round = 0,
     board = [[], [], []],
-    scores = [0, 0]
+    scores = [0, 0],
+    lineSum = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0],
+    ]
 
 let boardElement, restartElement, squaresElement, playersElement, mainElement
 
 function loadGame() {
-    // Get users
-    let users = new URLSearchParams(window.location.search).getAll("user")
-    if (!users || users.length !== 2) users = ["player 1", "player 2"]
-
     // Load elements
     boardElement = document.getElementById("board")
     mainElement = document.querySelector("main")
     restartElement = document.getElementById("restart")
     squaresElement = Array.from(document.getElementsByClassName("square"))
-    playersElement = Array.from(document.querySelectorAll("#player1, #player2"))
+    playersElement = Array.from(document.querySelectorAll("#user, #pc"))
 
-    // Read the username
-    playersElement.map((player, index) => {
-        player.children[0].innerHTML = users[index]
-    })
+    // Set the username
+    playersElement[0].children[0].innerHTML = new URLSearchParams(window.location.search).get("user") || "Player"
 
     // Reset button
     restartElement.addEventListener("click", resetGame)
@@ -45,41 +44,58 @@ function resetGame() {
 
     boardElement.classList.remove("reset")
     restartElement.style.display = "none"
+    lineSum = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0],
+    ]
     round = 0
+
+    if (playerTurn === 1) pcPlay()
 }
 
 function play(x, y) {
     const target = board[x][y]
-    if (target.player !== 0) return
+    if (target.player !== 0) return false
 
     target.player = [-1, 1][playerTurn]
     target.classList.add(["o", "x"][playerTurn], "clicked")
 
     round++
-    verifyBoard(x, y)
+    const win = verifyBoard(x, y)
 
     playerTurn = ++playerTurn % 2
-    mainElement.classList.toggle("oturn")
-    mainElement.classList.toggle("xturn")
+    mainElement.classList.toggle("userTurn")
+    mainElement.classList.toggle("pcTurn")
+
+    if (!win && playerTurn === 1) pcPlay()
+    return true
+}
+
+function sumBoard(x, y) {
+    lineSum[0][x] = board[x].reduce((acc, curr) => acc + curr.player, 0)
+    lineSum[1][y] = board.reduce((acc, curr) => acc + curr[y].player, 0)
+    lineSum[2][0] = board.reduce((acc, curr, index) => acc + curr[index].player, 0)
+    lineSum[2][1] = board.reduce((acc, curr, index) => acc + curr[2 - index].player, 0)
 }
 
 function verifyBoard(x, y) {
-    const verifyLines = [
-        board[x].reduce((acc, curr) => acc + curr.player, 0),
-        board.reduce((acc, curr) => acc + curr[y].player, 0),
-        board.reduce((acc, curr, index) => acc + curr[index].player, 0),
-        board.reduce((acc, curr, index) => acc + curr[2 - index].player, 0),
-    ].some((sum, type) => checkLine(sum, type, x, y))
+    sumBoard(x, y)
 
-    if (!verifyLines && round === 9) tie()
+    const win = [
+        [0, x],
+        [1, y],
+        [2, 0],
+        [2, 1],
+    ].some(([a, b], type) => checkLine(lineSum[a][b], type, x, y))
+
+    if (win) return true
+    if (round === 9) return tie()
+    return false
 }
 
 function checkLine(sum, type, x, y) {
-    if (sum === 3 || sum === -3) {
-        win(type, x, y)
-        return true
-    }
-
+    if (sum === 3 || sum === -3) return win(type, x, y)
     return false
 }
 
@@ -95,17 +111,55 @@ function win(type, x, y) {
 
     playersElement[playerTurn].children[1].innerHTML = ++scores[playerTurn]
 
-    gameOver()
+    return gameOver()
 }
 
 function tie() {
     squaresElement.map((border) => border.classList.add("tie"))
-    gameOver()
+    return gameOver()
 }
 
 function gameOver() {
     boardElement.classList.add("reset")
     restartElement.style.display = "block"
+    return true
+}
+
+function pcPlay() {
+    if (pcVerifyBoard()) return
+    if (play(1, 1)) return
+
+    let x, y
+    do {
+        x = Math.floor(Math.random() * 3)
+        y = Math.floor(Math.random() * 3)
+    } while (!play(x, y))
+}
+
+function pcVerifyBoard() {
+    for (let op = 2; op >= -2; op -= 4) {
+        for (let i = 0; i < 3; i++) {
+            if (lineSum[0][i] === op) {
+                return play(i, 0) || play(i, 1) || play(i, 2)
+            }
+        }
+
+        for (let j = 0; j < 3; j++) {
+            if (lineSum[1][j] === op) {
+                return play(0, j) || play(1, j) || play(2, j)
+            }
+        }
+
+        if (lineSum[2][0] === op) {
+            return play(0, 0) || play(1, 1) || play(2, 2)
+        }
+
+        if (lineSum[2][1] === op) {
+            return play(2, 0) || play(1, 1) || play(0, 2)
+        }
+    }
+
+    return false
 }
 
 loadGame()
